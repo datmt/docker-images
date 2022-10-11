f231caa5f783487bdc6188e709a134a721213c3a
dtt <dtt@local.com>
2022-10-11 23:08:05 +0700

    add convenient build script

---
+++ activemq/5.16.5/build-11.sh
@@ -0,0 +1,1 @@
+ docker build -t codingpuss/activemq:5.16.5-jre-11-alpine -f Dockerfile-jre-11-alpine .
---
+++ activemq/5.16.5/build-19.sh
@@ -0,0 +1,1 @@
+ docker build -t codingpuss/activemq:5.16.5-jre-19-alpine -f Dockerfile-jre-19-alpine .
